233f4060a2cd62c13b75237b7596fbeeefe29737
Alessandro <the.solarstone@gmail.com>
2019-07-27 07:47:38 +0300

    test to amend 2

--- bond_db/settings/local.py
+++ bond_db/settings/local.py
@@ -1,6 +1,9 @@
 from .base import *
+import os
 
 DEBUG = True
+
+SECRET_KEY = '0nvp)2o93(=7f$j5siuu9e$v-w^2_ho7*fpiitfyfuh(*1)v34'
 
 ALLOWED_HOSTS = []
 
--- bond_db/settings/pro.py
+++ bond_db/settings/pro.py
@@ -1,8 +1,12 @@
 from .base import *
+import os
 
 DEBUG = False
 
+assert SECRET_KEY is not None, ('Provide DJANGO_SECRET_KEY')
+
 ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS += [os.getenv('DJANGO_ALLOWED_HOSTS'),]
 
 DATABASES = {
     'default': {
