740c243b71510a1881b54751529eef028af9ef10
Alessandro <the.solarstone@gmail.com>
2019-08-19 14:12:50 +0300

    more config

--- run_uwsgi_nginx.sh
+++ run_uwsgi_nginx.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+sudo service postgresql start
+sudo service nginx start
 source ~/webdev/bin/activate
-uwsgi --ini config/uwsgi.ini #config/uwsgi-single.ini
+uwsgi --ini config/uwsgi.ini
+
 deactivate
 echo 'Venv deactivated'
+sudo service postgresql stop
+sudo service nginx stop
+echo 'PG and nginx services stopped'
