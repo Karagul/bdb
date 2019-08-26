1debaaccbac01846d22ca9addd028aebfc0649a4
Alexander <the.solarstone@gmail.com>
2019-08-26 21:31:20 +0300

    Update pro.py

--- bond_db/settings/pro.py
+++ bond_db/settings/pro.py
@@ -13,7 +13,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'bonds_db',
         'USER': 'analyst',
-        'PASSWORD': 'downdown',
+        'PASSWORD': 'fakepassword',
         'HOST': 'localhost',
         'PORT': '5432',
     }
